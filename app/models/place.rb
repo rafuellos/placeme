07cd0c4cdc2a398aef01c67e62d7eb15c340e450
Rafael Coomonte <rafuellos@gmail.com>
2015-08-18 10:14:20 +0200

    Established the model relations between user and owned places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ActiveRecord::Base
+  belongs_to :owner, class_name: 'User'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817085139) do
+ActiveRecord::Schema.define(version: 20150818075857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150817085139) do
   create_table "places", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "owner_id"
   end
 
   create_table "users", force: :cascade do |t|
