b40d57c5b6510c59e1a3c8736b80bf517dbb316f
Rafael Coomonte <rafuellos@gmail.com>
2015-08-19 11:20:56 +0200

    Minor change to default image paperclip, not for this branch sorry

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,7 @@ class Place < ActiveRecord::Base
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   validates :owner,  presence: true
 
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing.png"
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
 end
