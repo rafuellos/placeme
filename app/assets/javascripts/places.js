abadf00eb3c078f40a102bafc3ea5be402fe828f
Rafael Coomonte <rafuellos@gmail.com>
2015-08-24 13:11:19 +0200

    Marker and address changes in current location, bug in the marker when select the current location. More changes in css layout

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,7 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $( document ).ready(function() {
-
+  var lat;
+  var lon;
+  var map;
+  var marker;
+  var geocoder;
+  var mapOptions = {
+                      center: new google.maps.LatLng(0.0, 0.0),
+                      zoom: 2,
+                      mapTypeId: google.maps.MapTypeId.ROADMAP 
+                    };
+  
   if ("geolocation" in navigator) {
       getLocation();
     } else {
@@ -16,82 +26,73 @@ $( document ).ready(function() {
 
   function getLocation() {
     console.log('Getting location...'); 
-    navigator.geolocation.getCurrentPosition(setUserLocation, onError, options);
-  }
+    navigator.geolocation.getCurrentPosition(setLocation, onError, options);
+    var coord = {
+                  latitude: lat,
+                  longitude: lon
+                };
 
-  function setUserLocation (position) {
-    console.log("Got it!");
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    var coord = {latitude: lat,
-                 longitude: lon
-               };
-    console.log(coord);
     $.get('/users/set_coordinates', coord).done(
       function(result) {
         console.log('enviando al current_user');
       }
-    ).fail(
+      ).fail(
       console.log('problems')
-    )
+      );
   }
+
+  function setLocation (position) {
+    console.log("Got it!");
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    console.log(lat);
+    console.log(lon);
+    $('#latitude-place').val(lat);
+    $('#longitude-place').val(lon);
+  }
+
 
   function onError(error) {
     console.log("Getting location failed: " + error);
   }
 
-  $('#modal-add').on('click', '#set_location', function(event){
-    navigator.geolocation.getCurrentPosition(placeLocation, onError, options);
-  });
 
-  function placeLocation (position) {
-    console.log("Got it!");
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    $('#longitude-place').val(lon);
-    $('#latitude-place').val(lat);
-    //displayMap(lat,lon)
-  }
-
-  function displayMap(lat, lon) {
-    var urlRoot = "https://maps.googleapis.com/maps/api/staticmap?center=";
-    var urlParams = "&zoom=13&size=400x300";
-    var url = urlRoot + lat + "," + lon + urlParams;
-    var map = $('map-for-location');
-    map.setAttribute("src", url);
-    console.log(url);
-  }
 
   $('#modal-add').on('loaded.bs.modal', function(){
-  console.log('showing the modal')    
+    console.log('showing the modal')    
     initMap();
+
+    $('#modal-add').on('click', '#set_location', function(event){
+      initMap();
+    });
   });
 
 
-  var map;
-  var geocoder;
-  var mapOptions = { center: new google.maps.LatLng(0.0, 0.0), zoom: 2,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP };
 
   function initMap() {
-    console.log('paint the map?')
-    console.log($("#map_canvas").val()); 
+
+    navigator.geolocation.getCurrentPosition(setLocation, onError, options);
+   
     var myOptions = {
-                      center: new google.maps.LatLng(36.835769, 10.247693 ),
-                      zoom: 15,
+                      center: new google.maps.LatLng(lat, lon ),
+                      zoom: 14,
                       mapTypeId: google.maps.MapTypeId.ROADMAP
                     };
 
     geocoder = new google.maps.Geocoder();
     var map = new google.maps.Map(document.getElementById("map_canvas"),
     myOptions);
-    console.log(map);
+
+
     google.maps.event.addListener(map, 'click', function(event) {
+      console.log(event.latLng)
       placeMarker(event.latLng);
     });
+    console.log(map.center)
+    placeMarker(map.center);
 
-    var marker;
     function placeMarker(location) {
+
       if(marker){ 
           marker.setPosition(location);
       }else{
