b360aa2d66acc057e5cc67482fd3fb8142a4dd14
Rafael Coomonte <rafuellos@gmail.com>
2015-08-24 18:48:37 +0200

    Added a overlay for signin purposes

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,68 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 280) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+
+
+  function openOverlay(olEl) {
+    $oLay = $(olEl);
+    $window = $(window)
+        
+    if ($('#overlay-shade').length == 0)
+      $('body').prepend('<div id="overlay-shade"></div>');
+
+      $('#overlay-shade').fadeTo(400, 0.6, function() {
+            var props = {
+                          oLayWidth       : $oLay.width(),
+                          scrTop          : $window.scrollTop(),
+                          viewPortWidth   : $window.width()
+                        };
+
+      var leftPos = (props.viewPortWidth - props.oLayWidth) / 2;
+
+            $oLay
+                .css({
+                    display : 'block',
+                    opacity : 0,
+                    top : '-=300',
+                    left : leftPos+'px'
+                })
+                .animate({
+                    top : props.scrTop + 40,
+                    opacity : 1
+                }, 600);
+        });
+    }
+
+    function closeOverlay() {
+        $('.overlay').animate({
+            top : '-=300',
+            opacity : 0
+        }, 400, function() {
+            $('#overlay-shade').fadeOut(300);
+            $(this).css('display','none');
+        });
+    }
+    
+    $('body').on('click','#overlay-shade, .overlay a', function(e) {
+        closeOverlay();
+        if ($(this).attr('href') == '#') e.preventDefault();
+    });
+    
+    
+    // Usage
+    $('#overlaylaunch-inAbox').click(function(e) {
+       openOverlay('#overlay-inAbox');
+       e.preventDefault();
+    });
+
+
+});
