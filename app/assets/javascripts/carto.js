4a68a7751005466f3e70315272773b136c00db07
Rafael Coomonte <rafuellos@gmail.com>
2015-08-26 10:17:26 +0200

    SQL insertion in cartodb table ok

--- app/assets/javascripts/carto.js
+++ app/assets/javascripts/carto.js
@@ -6,62 +6,38 @@
 
 $( document ).ready(function() {
   var cartoKey = "e4abaae0cf1e668af417d8053aafee7a064aeb61";
-  $('body').on('click', '#testing-carto', function(){
-    console.log('Creando un place');
 
-    var sqlStatement = "INSERT INTO places_table (title, comments, the_geom) VALUES ('prueba2', 'prueba coment2', ST_SetSRID(ST_Point(-110, 43),4326))"
+  $('body').on('submit', '.add-place-form', function(event){
 
-    var url = "https://rafuellos.cartodb.com/api/v2/sql?q=" 
-              + sqlStatement 
-              + "&api_key=" + cartoKey
-    console.log(url);
-    $.post(url
-      , function(data) {
-        console.log(data)
+     var title = $('#form-tittle-place').val();
+     var comments = $('#form-comments-place').val();
+     var user = $('#form-user-place').attr('value');
+     var address = $('#address-place').val();
+     var longitudeForm = $('#longitude-place').val();
+     var latitudeForm = $('#latitude-place').val();
+     
+
+     console.log(title);
+     console.log(comments);
+     console.log(user);
+     console.log(address);
+     console.log(longitudeForm);
+     console.log(latitudeForm);
+
+      var sqlStatement1 = "INSERT INTO places_table (title, comments, owner_id, lon, lat, address, the_geom) ";
+      var sqlStatement2 = "VALUES ('" + title + "','" + comments + "'," + user + ",'" + longitudeForm + "','" + latitudeForm + "','" + address + "', ST_SetSRID(ST_Point(" + longitudeForm + "," + latitudeForm + "),4326))";
+      var url = sqlStatement1 + sqlStatement2;
+      console.log(url);
+
+      var urlcomplete = "https://rafuellos.cartodb.com/api/v2/sql?q=" 
+              + sqlStatement1 + sqlStatement2 + "&api_key=" + cartoKey;
+      console.log(urlcomplete);
+      $.post(urlcomplete, function(data) {
+        console.log(data);
     });
   });
 
-
-  $('body').on('submit', '.add-place-form', function(event){
-     console.log($('#form-tittle-place').val());
-     
-    var place = JSON.stringify($( this ).serializeObject());
-    //$('#modal-add').modal('toggle');
-    console.log('DENTRO' + place);
-    console.log('Titulo del place ' + place.comments);
-
-    // var request = $.post('/places/create', place);
-
-    // function onSaveSuccess(response) {
-    //   console.debug('BOOM', response);
-    // }
-
-    // function onSaveFailure(err) {
-    //   console.error(err.responseJSON);
-    // }
-
-    // request.done(onSaveSuccess);
-    // request.fail(onSaveFailure);
-
-  });
-
-  $.fn.serializeObject = function (){
-      var object = {};
-      var form = this.serializeArray();
-      $.each(form, function() {
-          if (object[this.name] !== undefined) {
-              if (!object[this.name].push) {
-                  object[this.name] = [object[this.name]];
-              }
-              object[this.name].push(this.value || '');
-          } else {
-              object[this.name] = this.value || '';
-          }
-      });
-      return object;
-  };
-
-
+  
 
 
 });
